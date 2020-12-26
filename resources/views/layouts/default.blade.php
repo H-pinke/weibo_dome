34593c9448a80571a915144d17829b419bcc0c05
huyaohua <huyaohua@258.com>
2020-12-26 17:12:41 +0800

    初始化

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,23 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <title>@yield('title', 'Weibo App') -- Laravel</title>
-    </head>
-    <body>
-        @yield('content')
-    </body>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Weibo App') - Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="/help">帮助</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">登录</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">@yield('content')</div>
+</body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>主页1</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是
+            <a href="#"></a>
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class=" btn btn-lg btn-success"href="#"role="button">现在注册</a>
+        </p>
+    </div>
 @stop
