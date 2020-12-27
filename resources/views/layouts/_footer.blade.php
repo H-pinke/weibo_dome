df847c79135a7251fe78dd73bfd7707faa295552
huyaohua <huyaohua@258.com>
2020-12-27 15:59:40 +0800

    局部布局，注册页面

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <a href=" https://huyaohua.top" target=_blank>
+        刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="{{ route('about') }}">关于</a>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('name');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+
+Route::get('/signup', 'UsersController@create')->name('signup');
